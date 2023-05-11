ffee3e821d2251cdfa93d4a38581903d3b4038df
Ronak Agarwal <agarwalronak58@gmail.com>
2023-05-11 16:44:31 -0400

    New repo

---
+++ lib/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Container(
+          child: Center(
+            child: Text('Hello, world!'),
+          ),
+        ),
+      ),
+    );
+  }
+}
